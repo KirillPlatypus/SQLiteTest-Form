8122599e3c77120324491c080ddcc70f25b5cb81
Kirill <yarovoykirill942@gmail.com>
2020-09-28 21:13:57 +0300

    Add files

---
+++ SQLite3Test/ConnectDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
+
+namespace SQLite3Test
+{
+    class ConnectDB
+    {
+        public SQLiteConnection connect;
+
+        public ConnectDB(string path)
+        {
+            connect = new SQLiteConnection(path);
+            if (!File.Exists(path))
+            {
+                SQLiteConnection.CreateFile("PeopleList.sqlite3");
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if(connect.State != ConnectionState.Open)
+            {
+                connect.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (connect.State != ConnectionState.Closed)
+            {
+                connect.Close();
+            }
+        }
+
+    }
+}
---
+++ SQLite3Test/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SQLite;
+
+
+namespace SQLite3Test
+{
+    class Program
+    {
+        static PeopleToday today = new PeopleToday();
+        static ConnectDB connectDB ;
+        static void Main(string[] args)
+        {
+            connectDB = new ConnectDB(today.Path);
+
+            connectDB.OpenConnection();
+
+            ReadDB();
+            ChangeDB();
+            ReadDB();
+
+            connectDB.CloseConnection();
+        }
+        private static void ReadDB()
+        {
+            var command = new SQLiteCommand("SELECT * FROM PeopleToday", connectDB.connect);
+
+            var result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    today = new PeopleToday()
+                    {
+                        id = (long)result["Id"],
+                        name = result["Name"].ToString(),
+                        app = result["App"].ToString(),
+                        date = (long)result["Date"]
+                    };
+                    today.Message();
+
+                }
+
+            }
+        }
+        private static void ChangeDB()
+        {
+            var command = new SQLiteCommand("UPDATE PeopleToday SET Date = '800' WHERE Id = 4; ", connectDB.connect);
+            command.Parameters.AddWithValue("@id", 4);
+            command.Parameters.AddWithValue("@Name", "Lara");
+            command.Parameters.AddWithValue("@App", "DrakeHub");
+            command.Parameters.AddWithValue("@Date", 900);
+            
+            command.ExecuteReader();
+        }
+
+    }
+}
